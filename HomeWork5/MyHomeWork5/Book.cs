b36a59a3e2ac10318008756cc5b4101d3977b74e
Levan Khositashvili <103431465+khositashvili@users.noreply.github.com>
2024-10-22 01:00:09 +0400

    Final Home work 5

--- HomeWork5/MyHomeWork5/Book.cs
+++ HomeWork5/MyHomeWork5/Book.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +14,10 @@ namespace MyHomeWork5
         //Fileds
         private string _title;
         private string _author;
-        private int _isbn;
+        private string _isbn;
+        private int _totalCopies;
         private int _copiesAvailable;
-
+        
         //Propertes
         public string Title {
             get { return _title; }
@@ -25,9 +28,11 @@ namespace MyHomeWork5
             get { return _author; }
             set { _author = value; }
         }
-        public int ISBN
+        public string ISBN { get; set; } //Only set??? DisplayInfo get-is გარეშე როგორ წამოიღებს მერე?
+        public int TotalCopies
         {
-            set { _isbn = value; }
+            get { return _totalCopies; }
+            set { _totalCopies = value; }
         }
         public int CopiesAvailable
         {
@@ -36,50 +41,118 @@ namespace MyHomeWork5
         }
 
         //Constructor
-        public Book (string title, string author, int isbn, int copiesAvailable)
+        public Book (string title, string author, string isbn, int totalCopies)
         {
             Title = title;
             Author = author;
             ISBN = isbn;
-            CopiesAvailable = copiesAvailable;
+            TotalCopies = totalCopies;
+            CopiesAvailable = totalCopies;
         }
 
         //Methods
-        public void DissplayInfo()
+        public void DisplayInfo()
         {
-
+            Console.WriteLine($"\"{Title}\" by {Author} (ISBN:{ISBN}). Copies available: {TotalCopies}");
         }
+
         public void BorrowBook()
         {
-
+            if (CopiesAvailable > 0)
+            {
+                CopiesAvailable--; // Reduce the number of available copies.
+                Console.WriteLine($"You have successfully borrowed \"{Title}\" by {Author}. Copies left: {CopiesAvailable}");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, no copies of \"{Title}\" are available.");
+            }
         }
+
         public void ReturnBook()
         {
-            _isbn ++;
+            if (CopiesAvailable < TotalCopies)
+            {
+                CopiesAvailable++; // Increase the number of available copies.
+                Console.WriteLine($"You have successfully returned \"{Title}\" by {Author}. Available copies: {CopiesAvailable}");
+            }
+            else
+            {
+                Console.WriteLine($"All copies of \"{Title}\" are already in the library.");
+            }
         }
 
     }
 
     public class Library
     {
-        private string books {  get; set; }
-    
-        public void AddBook ()
-        {
+        //Field
+        private List<Book> books;
 
-        }
-        public void RemoveBook()
+        // Constructor
+        public Library()
         {
-
+            books = new List<Book>();
         }
+        //Methods
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+            Console.WriteLine($"Book \"{book.Title}\" by {book.Author} added to the library.");
+        }
+        public void RemoveBook(string title)
+        {
+            Book bookToRemove = books.Find(b => b.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+
+            if (bookToRemove != null)
+            {
+                books.Remove(bookToRemove);
+                //აქ მინდოდა რომ დაკლებოდა წიგნების TotalCopies რომ დაკლებოდა, მაგრამ ვერ ვქენი :(
+                Console.WriteLine($"Book \"{title}\" has been removed from the library.");
+            }
+            else
+            {
+                Console.WriteLine($"Book \"{title}\" not found in the library.");
+            }
+        }
+
         public void DisplayAllBooks()
         {
-
+            if (books.Count == 0)
+            {
+                Console.WriteLine("No books available in the library.");
+            }
+            else
+            {
+                Console.WriteLine("Books in the library:");
+                foreach (var book in books)
+                {
+                    book.DisplayInfo();
+                    Console.WriteLine(); //Blank line between books
+                }
+            }
         }
 
+        public void Search(string query)
+        {
+            List<Book> searchResults = books.FindAll(b =>
+                b.Title.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                b.Author.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0);
 
-
+            if (searchResults.Count > 0)
+            {
+                Console.WriteLine($"Search results for \"{query}\":");
+                foreach (var book in searchResults)
+                {
+                    book.DisplayInfo();
+                    Console.WriteLine(); // Blank line between results
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Book \"{query}\" is not  in library.");
+            }
+        }
     }
-    
 
 }
--- HomeWork5/MyHomeWork5/Program.cs
+++ HomeWork5/MyHomeWork5/Program.cs
@@ -1,11 +1,30 @@
 ﻿using MyHomeWork5;
 
-static void task1()
+static void main()
 {
-    Book book1 = new Book("Gantskobis Phsycology", "Dimitri Uznadze", 1000000, 100);
-    Book book2 = new Book("Gantskoba", "Dimitri Uznadze", 1000001, 50);
-    Console.WriteLine(book1.Author);
+    Library library = new Library();
+    Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", "00001", 2);
+    Book book2 = new Book("The Book Thief", " Markus Zusak", "00002", 3);
+    Book book3 = new Book("The Hunger Games", " Suzanne Collins", "00003", 4);
+    Book book4 = new Book("Harry Potter and the Sorcerer’s Stone(#1)", "J.K. Rowling", "00004", 5);
 
+    //book1.DisplayInfo();
+    //book2.DisplayInfo();
+    //book3.DisplayInfo();
+    //book4.DisplayInfo();
+    //book1.BorrowBook();
+    //book1.BorrowBook();
+    //book1.BorrowBook();
+    //book1.BorrowBook(); //Sorry, no copies of "To Kill a Mockingbird" are currently available.
+    //book1.ReturnBook(); //You have successfully returned "To Kill a Mockingbird" by Harper Lee. Available copies: 1
+
+    //library.AddBook(book1);
+    //library.AddBook(book2);
+    library.AddBook(book3);
+    library.AddBook(book4);
+    library.RemoveBook("Harry Potter and the Sorcerer’s Stone(#1)");
+    library.DisplayAllBooks();
+    library.Search("Suzanne Collins");
 }
 
-task1();
+main();
